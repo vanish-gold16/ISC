73ff771c3443eb30281796e0340c0ff7f7361d64
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-02-12 09:07:34 +0100

    feat: initial HTML template, HomeController, User model, and Thymeleaf dependency

--- src/main/java/org/example/isc/main/home/HomeController.java
+++ src/main/java/org/example/isc/main/home/HomeController.java
@@ -1,12 +1,21 @@
 package org.example.isc.main.home;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("title", "Home");
 
+        return "home";
+    }
 
 }
---
+++ src/main/java/org/example/isc/main/models/User.java
@@ -0,0 +1,43 @@
+package org.example.isc.main.models;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "current_study")
+    private String currentStudy;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getCurrentStudy() {
+        return currentStudy;
+    }
+
+    public void setCurrentStudy(String currentStudy) {
+        this.currentStudy = currentStudy;
+    }
+}
