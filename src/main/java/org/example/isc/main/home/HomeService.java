5d75df88a99307c8bef756bfd16d563c10c113ee
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-02-12 09:51:10 +0100

    feat: add getSubscriptionList in HomeService.java

--- src/main/java/org/example/isc/main/home/HomeService.java
+++ src/main/java/org/example/isc/main/home/HomeService.java
@@ -1,10 +1,22 @@
 package org.example.isc.main.home;
 
+import org.example.isc.main.repositories.SubscriptionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 public class HomeService {
 
+    private static final Logger log = LoggerFactory.getLogger(HomeService.class);
+    private SubscriptionRepository subscriptionRepository;
 
+    public HomeService(SubscriptionRepository subscriptionRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+    }
+
+    public void getSubscriptionList(Long userId){
+        subscriptionRepository.findSubscriptionByFollowedId(userId);
+    }
 
 }
