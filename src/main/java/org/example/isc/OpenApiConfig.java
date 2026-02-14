954c40286d56ef14c96a499d908f5c40b9132157
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-02-14 22:04:35 +0100

    feat: add OpenAPI configuration for API documentation

---
+++ src/main/java/org/example/isc/OpenApiConfig.java
@@ -0,0 +1,18 @@
+package org.example.isc;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ISC")
+                        .version("1.0.0")
+                        .description("International Student Community"));
+    }
+}
