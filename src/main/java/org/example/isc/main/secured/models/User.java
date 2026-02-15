8eebbebb95892df998ff64e3addbf67ef052ceb2
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-02-15 20:27:27 +0100

    feat: update User model

--- src/main/java/org/example/isc/main/secured/models/User.java
+++ src/main/java/org/example/isc/main/secured/models/User.java
@@ -11,8 +11,20 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "username")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "username", nullable = false)
     private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String passwordHash;
 
     @Column(name = "current_study")
     private String currentStudy;
