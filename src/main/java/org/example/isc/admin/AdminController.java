48318582cb0c28950bd69e4ff509650f401cf9b7
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-02-15 19:21:25 +0100

    fix: wrong endpoints in reg log

--- src/main/java/org/example/isc/admin/AdminController.java
+++ src/main/java/org/example/isc/admin/AdminController.java
@@ -2,11 +2,13 @@ package org.example.isc.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping
+    @GetMapping("/management")
     public String getManagement(){
         return "private/admin/management";
     }
