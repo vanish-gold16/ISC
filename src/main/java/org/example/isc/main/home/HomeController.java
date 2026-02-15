1cb5e939968fd86de6ba769e00bc818b0b19bf54
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-02-15 13:16:53 +0100

    feat: add authentication styles and layout for login and registration pages

--- src/main/java/org/example/isc/main/home/HomeController.java
+++ src/main/java/org/example/isc/main/home/HomeController.java
@@ -3,9 +3,7 @@ package org.example.isc.main.home;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 @RequestMapping("/")
