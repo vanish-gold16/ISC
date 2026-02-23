08b66eca7ceea3378effef58e67f6d26e9f2c70a
Ivan Mytrofanov <128700493+vanish-gold16@users.noreply.github.com>
2026-02-23 18:08:23 +0100

    fix: in NotificationsRepository.java query

--- src/main/java/org/example/isc/main/secured/repositories/NotificationsRepository.java
+++ src/main/java/org/example/isc/main/secured/repositories/NotificationsRepository.java
@@ -3,9 +3,10 @@ package org.example.isc.main.secured.repositories;
 import org.example.isc.main.secured.models.Notification;
 import org.example.isc.main.secured.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,8 +14,14 @@ public interface NotificationsRepository extends JpaRepository<Notification, Lon
 
     List<Notification> findByRecieverOrderByCreatedAtDesc(User reciever, Pageable pageable);
 
-    long countByRecieverAndReadAtIsNull(User reciever, LocalDateTime readAt);
+    long countByRecieverAndReadAtIsNull(User reciever);
 
+    @Modifying
+    @Query("""
+        UPDATE Notification n\s
+                   SET n.readAt = CURRENT_TIMESTAMP\s
+                   WHERE n.reciever = :reciever AND n.readAt IS NULL
+                """)
     void markAllReadByReciever(User reciever);
 
 }
