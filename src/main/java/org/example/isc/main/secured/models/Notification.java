54c3e01b0ae58cb6e1d3bf2b6bd03a30321a9605
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-02-23 12:47:14 +0100

    feat: finish Notification.java

--- src/main/java/org/example/isc/main/secured/models/Notification.java
+++ src/main/java/org/example/isc/main/secured/models/Notification.java
@@ -1,8 +1,9 @@
 package org.example.isc.main.secured.models;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import org.hibernate.annotations.SQLJoinTableRestriction;
+import org.example.isc.main.enums.NotificationEnum;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "notifications")
@@ -12,10 +13,79 @@ public class Notification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long notificationId;
 
-
-    @JoinColumn(name = "reciever_id",  nullable = false)
+    @ManyToOne
+    @JoinColumn(name = "reciever_id", nullable = false)
     private User reciever;
 
+    @Column(name = "type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private NotificationEnum status;
 
+    @Column(name = "title", nullable = false)
+    private String title;
 
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "read_at")
+    private LocalDateTime readAt;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getReadAt() {
+        return readAt;
+    }
+
+    public void setReadAt(LocalDateTime readAt) {
+        this.readAt = readAt;
+    }
+
+    public Long getNotificationId() {
+        return notificationId;
+    }
+
+    public void setNotificationId(Long notificationId) {
+        this.notificationId = notificationId;
+    }
+
+    public User getReciever() {
+        return reciever;
+    }
+
+    public void setReciever(User reciever) {
+        this.reciever = reciever;
+    }
+
+    public NotificationEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(NotificationEnum status) {
+        this.status = status;
+    }
 }
