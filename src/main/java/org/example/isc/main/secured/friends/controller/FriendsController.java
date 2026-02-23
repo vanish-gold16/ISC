390d33e06ed6dd010a91d2f95c4b611e85eb6527
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-02-23 10:41:45 +0100

    fix: update FriendsController and public-header for authentication checks

--- src/main/java/org/example/isc/main/secured/friends/controller/FriendsController.java
+++ src/main/java/org/example/isc/main/secured/friends/controller/FriendsController.java
@@ -4,10 +4,12 @@ import org.example.isc.main.secured.models.User;
 import org.example.isc.main.secured.repositories.FriendsRepository;
 import org.example.isc.main.secured.repositories.UserRepository;
 import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 @RequestMapping("/friends")
 public class FriendsController {
 
@@ -32,7 +34,7 @@ public class FriendsController {
         model.addAttribute("title", "Friends");
         model.addAttribute("friends", friendsRepository.findAllByRecieverUser(me));
 
-        return "/private/friends";
+        return "private/friends";
     }
 
 }
