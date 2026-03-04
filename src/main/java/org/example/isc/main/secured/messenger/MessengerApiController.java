659817421acfd01b68b06bedb2c91bf612343de6
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-03-04 11:27:33 +0100

    feat: add getMyConversations

--- src/main/java/org/example/isc/main/secured/messenger/MessengerApiController.java
+++ src/main/java/org/example/isc/main/secured/messenger/MessengerApiController.java
@@ -1,17 +1,37 @@
 package org.example.isc.main.secured.messenger;
 
+import org.example.isc.main.secured.models.User;
+import org.example.isc.main.secured.models.messenger.Conversation;
+import org.example.isc.main.secured.repositories.UserRepository;
+import org.example.isc.main.secured.repositories.conversation.ConversationRepository;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/conversations")
 public class MessengerApiController {
 
-    @GetMapping
-    public List<ConversationDTO> getMyConversations(){
+    private final ConversationRepository conversationRepository;
+    private final UserRepository userRepository;
 
+    public MessengerApiController(ConversationRepository conversationRepository, UserRepository userRepository) {
+        this.conversationRepository = conversationRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping
+    public List<Conversation> getMyConversations(
+            Authentication authentication
+    ){
+        User me = userRepository.findByUsernameIgnoreCase(authentication.getName())
+                        .orElseThrow(() -> new IllegalStateException("Logged-in user not found: " + authentication.getName()));
+
+        return conversationRepository.findByMember(me);
     }
 
     @PostMapping("/direct")
