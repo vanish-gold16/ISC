77c9a0902f14b019c7e801149ad0553168ad57a4
Ivan Mytrofanov <mitrofanovvana20082@gmail.com>
2026-03-11 13:41:53 +0100

    feat: add fields for art details and review status in Review.java

--- src/main/java/org/example/isc/opuscore/models/Review.java
+++ src/main/java/org/example/isc/opuscore/models/Review.java
@@ -1,6 +1,7 @@
 package org.example.isc.opuscore.models;
 
 import jakarta.persistence.*;
+import org.example.isc.opuscore.enums.ArtTypeEnum;
 
 import java.util.List;
 
@@ -12,6 +13,26 @@ public class Review {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "art_type", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ArtTypeEnum type;
 
+    @Column(name = "is_review")
+    private Boolean isReview;
+
+    @Column(name = "art_name",  nullable = false)
+    private String artName;
+
+    @Column(name = "art_description")
+    private String artDescription;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "body")
+    private String body;
+
+    @Column(name = "value", nullable = false)
+    private Long value;
 
 }
